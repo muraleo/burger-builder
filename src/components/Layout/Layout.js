afefc9cc0ba851afb528de82ef9bcfac5c6a3d6a
muraleo <muraleo80923@gmail.com>
2018-10-06 21:53:40 -0400

    add prop type validation to burger ingredient component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import AUX from '../../hoc/Aux';
+import classes from './Layout.module.css'
 
 const Layout = (props) =>(
     <AUX>
         <div></div>
-        <main>{props.children}</main>
+        <main className={classes.Content}>
+            {props.children}
+        </main>
     </AUX>
 );
 
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import AUX from '../../hoc/Aux'
+
+class BurgerBuilder extends Component{
+    render(){
+        return(
+            <AUX>
+                <div>Burger</div>
+                <div>Build control</div>
+            </AUX>
+        )
+    }
+}
+
+export default BurgerBuilder;
