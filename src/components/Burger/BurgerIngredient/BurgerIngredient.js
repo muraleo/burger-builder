0dabad546e8d73194023c7d029b9ce9104edd361
muraleo <muraleo80923@gmail.com>
2018-10-06 23:13:04 -0400

    outputing ingredients dynamically

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -44,7 +44,7 @@ class BurgerIngredient extends Component{
     }
 };
 
-BurgerIngredient.PropTypes = {
+BurgerIngredient.propTypes = {
     type: PropTypes.string.isRequired
 }
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,29 @@
 import React, {Component} from 'react';
 import AUX from '../../hoc/Aux'
+import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component{
+    // can use constructor
+    // constructor(props){
+    //     super(props);
+    //     this.state = {
+
+    //     }
+    // }
+
+    state = {
+        ingredients:{
+            salad: 1,
+            nacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
+
     render(){
         return(
             <AUX>
-                <div>Burger</div>
+                <Burger ingredients = {this.state.ingredients}/>
                 <div>Build control</div>
             </AUX>
         )
