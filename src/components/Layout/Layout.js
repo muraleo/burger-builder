40d593004d3e466afa38ebf12d514004a5fa9ad9
muraleo <muraleo80923@gmail.com>
2018-10-08 17:42:16 -0400

    add sidebar toggle function

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,7 +6,7 @@ import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 class Layout extends Component {
 	state = {
-		showSideDrawer: true
+		showSideDrawer: false
 	};
 
 	sideDrawerClosedHandler = () => {
@@ -15,10 +15,16 @@ class Layout extends Component {
 		});
 	};
 
+	sideDrawerToggleHandler = () => {
+		this.setState(preState => {
+			return { showSideDrawer: !preState.showSideDrawer };
+		});
+	};
+
 	render() {
 		return (
 			<AUX>
-				<Toolbar />
+				<Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
 				<SideDrawer
 					open={this.state.showSideDrawer}
 					closed={this.sideDrawerClosedHandler}
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import Logo from '../../Logo/Logo'
-import Items from '../Items/Items'
-import classes from './Toolbar.module.css'
+import React from "react";
+import Logo from "../../Logo/Logo";
+import Items from "../Items/Items";
+import classes from "./Toolbar.module.css";
+import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
 
-const toolbar = (props) =>(
-    <header className={classes.Toolbar}>
-        <div>MENU</div>
-        <div className={classes.Logo}>
-            <Logo />
-        </div>
-        <nav className={classes.DesktopOnly}>
-            <Items />
-        </nav>
-    </header>
+const toolbar = props => (
+	<header className={classes.Toolbar}>
+		<DrawerToggle clicked={props.drawerToggleClicked} />
+		<div className={classes.Logo}>
+			<Logo />
+		</div>
+		<nav className={classes.DesktopOnly}>
+			<Items />
+		</nav>
+	</header>
 );
 
 export default toolbar;
