7fd73bf72c49780cf1e0f0c20d373ad6998af33c
muraleo <muraleo80923@gmail.com>
2018-10-13 13:57:50 -0400

    finish checkout component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import CheckoutSummary from "./containers/Checkout/Checkout";
 
 class App extends Component {
 	render() {
@@ -8,6 +9,7 @@ class App extends Component {
 			<div className="App">
 				<Layout>
 					<BurgerBuilder />
+					<CheckoutSummary />
 				</Layout>
 			</div>
 		);
---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,20 @@
+import React from "react";
+import classes from "./CheckoutSummary.module.css";
+
+import Burger from "../../Burger/Burger";
+import Button from "../../UI/Button/Button";
+
+const checkoutSummary = props => {
+	return (
+		<div className={classes.CheckoutSummary}>
+			<h1>We hope it tastes well!</h1>
+			<div className={classes.CheckoutBurger}>
+				<Burger ingredients={props.ingredients} />
+			</div>
+			<Button btnType="Danger">CANCEL</Button>
+			<Button btnType="Success">CONTINUE</Button>
+		</div>
+	);
+};
+
+export default checkoutSummary;
