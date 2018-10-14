17a40f7ebfda7aa403ad0494089cb70562a299a1
muraleo <muraleo80923@gmail.com>
2018-10-13 22:08:34 -0400

    set up react routes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React, { Component } from "react";
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import CheckoutSummary from "./containers/Checkout/Checkout";
+import { Route, Switch } from "react-router-dom";
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
 				<Layout>
-					<BurgerBuilder />
-					<CheckoutSummary />
+					<Switch>
+						<Route path="/checkout" component={CheckoutSummary} />
+						<Route path="/" component={BurgerBuilder} />
+					</Switch>
 				</Layout>
 			</div>
 		);
