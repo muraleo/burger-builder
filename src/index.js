896ba34b7c7a417720cb4227f9d60dbd2cad4e72
muraleo <muraleo80923@gmail.com>
2018-10-24 19:27:11 -0400

    install redux devtools

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, compose, applyMiddleware } from "redux";
 import reducer from "./store/reducer";
 
 import "./index.css";
@@ -9,7 +9,24 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 
-const store = createStore(reducer);
+const logger = store => {
+	return next => {
+		return action => {
+			console.log("[Middleware] Dispatching", action);
+			const result = next(action);
+			console.log("[Middleware] Next", store.getState());
+			return result;
+		};
+	};
+};
+
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const store = createStore(reducer, composeEnhancers(applyMiddleware(logger)));
+
+const store = createStore(
+	reducer,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
 	<Provider store={store}>
