433dbeb472c8e06f63cc2182812a3f6f09377467
muraleo <muraleo80923@gmail.com>
2018-10-29 15:36:40 -0400

    add asyncComponent to do lazy loading

--- src/index.js
+++ src/index.js
@@ -23,7 +23,10 @@ import { BrowserRouter } from "react-router-dom";
 // 	};
 // };
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers =
+	process.env.NODE_ENV === "development"
+		? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+		: null || compose;
 const rootReducer = combineReducers({
 	burgerBuilder: burgerBuilderReducer,
 	order: orderReducer,
