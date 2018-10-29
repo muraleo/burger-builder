286d47b8376fca5f09eb9ca08f99e51acf4d8740
muraleo <muraleo80923@gmail.com>
2018-10-29 10:41:43 -0400

    change autoLogout time

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -36,7 +36,7 @@ export const checkAuthTimeout = expirationTime => {
 	return dispatch => {
 		setTimeout(() => {
 			dispatch(authLogout());
-		}, expirationTime);
+		}, expirationTime * 1000);
 	};
 };
 
@@ -59,7 +59,6 @@ export const auth = (email, password, isSignIn) => {
 		axios
 			.post(url, authData)
 			.then(res => {
-				console.log(res);
 				const expirationDate = new Date(
 					new Date().getTime() + res.data.expiresIn * 1000
 				);
